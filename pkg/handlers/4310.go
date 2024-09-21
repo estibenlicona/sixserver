0ca05ba42a67b230076cf51edf4c0a959d37c216
estibenlicona <estibenlicona@gmail.com>
2024-09-21 07:42:56 -0500

    [gral]: fix by code review

--- pkg/handlers/4310.go
+++ pkg/handlers/4310.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Handle0x4310(pkt types.Packet, conn gnet.Conn) (out []byte, action gnet.Action) {
-	roomName := types.RemovePadding(pkt.Data[0:64])
+	var roomName = types.RemovePadding(pkt.Data[0:64])
 	lobby := types.Lobby{}
 
 	room := types.Room{
--- pkg/types/packet_header.go
+++ pkg/types/packet_header.go
@@ -16,10 +16,22 @@ func (header PacketHeader) String() string {
 	return fmt.Sprintf("PacketHeader(ID: %#x, Length: %d, PacketCount: %d)", header.ID, header.Length, header.PacketCount)
 }
 
-func (packetHeader PacketHeader) ToBytes() []byte {
+func (header PacketHeader) ToBytes() []byte {
 	buffer := new(bytes.Buffer)
-	binary.Write(buffer, binary.BigEndian, packetHeader.ID)
-	binary.Write(buffer, binary.BigEndian, packetHeader.Length)
-	binary.Write(buffer, binary.BigEndian, packetHeader.PacketCount)
+	err := binary.Write(buffer, binary.BigEndian, header.ID)
+	if err != nil {
+		fmt.Printf("Error writing ID: %v\n", err)
+	}
+
+	err = binary.Write(buffer, binary.BigEndian, header.Length)
+	if err != nil {
+		fmt.Printf("Error writing ID: %v\n", err)
+	}
+
+	err = binary.Write(buffer, binary.BigEndian, header.PacketCount)
+	if err != nil {
+		fmt.Printf("Error writing ID: %v\n", err)
+	}
+
 	return buffer.Bytes()
 }
